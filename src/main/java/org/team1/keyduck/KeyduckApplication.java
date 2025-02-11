b75f27b99125a4cc6010ad159427a7e67ee5ce83
star-pooh <eunki92@gmail.com>
2025-02-11 14:29:54 +0900

    init : Init Keyduck Project - Keyduck 프로젝트 생성

---
+++ src/main/java/org/team1/keyduck/KeyduckApplication.java
@@ -0,0 +1,13 @@
+package org.team1.keyduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyduckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyduckApplication.class, args);
+    }
+
+}
