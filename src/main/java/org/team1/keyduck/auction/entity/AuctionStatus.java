fc5ec57b634c27c78ea37b1070e8c06aa58ae0a6
star-pooh <eunki92@gmail.com>
2025-02-11 17:50:25 +0900

    init : Add Project Common File - 프로젝트 뼈대에서 필요한 공통 파일 추가

---
+++ src/main/java/org/team1/keyduck/auction/entity/AuctionStatus.java
@@ -0,0 +1,7 @@
+package org.team1.keyduck.auction.entity;
+
+public enum AuctionStatus {
+    NOT_STARTED,
+    IN_PROGRESS,
+    CLOSED
+}
---
+++ src/main/java/org/team1/keyduck/bidding/dto/response/BiddingResponseDto.java
@@ -0,0 +1,8 @@
+package org.team1.keyduck.bidding.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class BiddingResponseDto {
+
+}
