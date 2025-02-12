0480d0299f4e774c73c23207cfde9c1c71386215
Jiwon <may0399@naver.com>
2025-02-12 17:48:32 +0900

    Feat : 키보드 생성 로직을 ApiResponse 형식으로 변경

--- src/main/java/org/team1/keyduck/keyboard/controller/KeyboardController.java
+++ src/main/java/org/team1/keyduck/keyboard/controller/KeyboardController.java
@@ -1,14 +1,15 @@
 package org.team1.keyduck.keyboard.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.team1.keyduck.common.dto.ApiResponse;
+import org.team1.keyduck.common.exception.SuccessCode;
 import org.team1.keyduck.keyboard.dto.request.KeyboardCreateRequestDto;
-import org.team1.keyduck.keyboard.dto.response.KeyboardCreateResponseDto;
 import org.team1.keyduck.keyboard.service.KeyboardService;
 
 @RestController
@@ -19,11 +20,12 @@ public class KeyboardController {
 
     // 키보드 생성 API
     @PostMapping
-    public ResponseEntity<KeyboardCreateResponseDto> createKeyboardAPI(
-            @RequestBody KeyboardCreateRequestDto requestDto) {
+    public ResponseEntity<ApiResponse<Void>> createKeyboardAPI(
+            @RequestBody @Valid KeyboardCreateRequestDto requestDto) {
 
-        KeyboardCreateResponseDto ResponseDto = keyboardService.createKeyboard(requestDto);
-        return ResponseEntity.ok(ResponseDto);
+        keyboardService.createKeyboard(requestDto);
+        return new ResponseEntity<>(ApiResponse.success(SuccessCode.CREATE_SUCCESS),
+                SuccessCode.CREATE_SUCCESS.getStatus());
     }
 
 }
