ac246e0d99dbbe248da7104e5c934e4bf737be44
ijnow <135947130+ijnow@users.noreply.github.com>
2025-02-12 21:21:27 +0900

    Fix : NotBlank로 변경

--- src/main/java/org/team1/keyduck/keyboard/dto/request/KeyboardCreateRequestDto.java
+++ src/main/java/org/team1/keyduck/keyboard/dto/request/KeyboardCreateRequestDto.java
@@ -1,20 +1,19 @@
 package org.team1.keyduck.keyboard.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class KeyboardCreateRequestDto {
 
-    @NotNull(message = "")
+    @NotNull(message = "회원 Id값을 입력해주세요.")
     private Long memberId;
 
-    @NotNull(message = "상품 이름을 입력해주세요.")
+    @NotBlank(message = "상품 이름을 입력해주세요.")
     private String name;
 
-    @NotNull(message = "상품 설명을 입력해주세요.")
+    @NotBlank(message = "상품 설명을 입력해주세요.")
     private String description;
 
 
