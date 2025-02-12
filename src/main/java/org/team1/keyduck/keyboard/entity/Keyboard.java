f23ebb2d06d408372c9084d3cf26425b0e41b271
heehyun0221 <psr4580@gmail.com>
2025-02-12 10:28:46 +0900

    🚑 fix : Id 어노테이션 문제 수정

--- src/main/java/org/team1/keyduck/keyboard/entity/Keyboard.java
+++ src/main/java/org/team1/keyduck/keyboard/entity/Keyboard.java
@@ -5,13 +5,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 import org.team1.keyduck.common.entity.BaseTime;
 import org.team1.keyduck.member.entity.Member;
 
