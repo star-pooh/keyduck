f0d300bfa178d0af0fdc04415cc324fb83730d6e
Jiwon <may0399@naver.com>
2025-02-12 17:48:32 +0900

    Fix : 키보드 생성 로직을 ApiResponse 형식으로 변경

--- src/main/java/org/team1/keyduck/keyboard/dto/request/KeyboardCreateRequestDto.java
+++ src/main/java/org/team1/keyduck/keyboard/dto/request/KeyboardCreateRequestDto.java
@@ -1,12 +1,21 @@
 package org.team1.keyduck.keyboard.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class KeyboardCreateRequestDto {
+
+    @NotNull(message = "")
     private Long memberId;
+
+    @NotNull(message = "상품 이름을 입력해주세요.")
     private String name;
+
+    @NotNull(message = "상품 설명을 입력해주세요.")
     private String description;
+
+
 }
--- src/main/java/org/team1/keyduck/keyboard/service/KeyboardService.java
+++ src/main/java/org/team1/keyduck/keyboard/service/KeyboardService.java
@@ -3,8 +3,9 @@ package org.team1.keyduck.keyboard.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.team1.keyduck.common.exception.DataNotFoundException;
+import org.team1.keyduck.common.exception.ErrorCode;
 import org.team1.keyduck.keyboard.dto.request.KeyboardCreateRequestDto;
-import org.team1.keyduck.keyboard.dto.response.KeyboardCreateResponseDto;
 import org.team1.keyduck.keyboard.entity.Keyboard;
 import org.team1.keyduck.keyboard.repository.KeyboardRepository;
 import org.team1.keyduck.member.entity.Member;
@@ -19,25 +20,18 @@ public class KeyboardService {
 
     // 키보드 생성
     @Transactional
-    public KeyboardCreateResponseDto createKeyboard(KeyboardCreateRequestDto requestDto) {
+    public void createKeyboard(KeyboardCreateRequestDto requestDto) {
         // 저장한 유저아이디 가져오기(추후 토큰 적용)
 
         Member member = memberRepository.findById(requestDto.getMemberId())
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+                .orElseThrow(() -> new DataNotFoundException(ErrorCode.USER_NOT_FOUND));
 
         Keyboard keyboard = Keyboard.builder()
                 .member(member)
                 .name(requestDto.getName())
-                .description(requestDto.getDescription())
                 .build();
 
         keyboardRepository.save(keyboard);
-        return new KeyboardCreateResponseDto(keyboard.getId(), keyboard.getName(), keyboard.getDescription());
     }
 
-//    public void test() {
-//        keyboardRepository.findById(1L).orElseThrow(
-//                () -> new DataNotFoundException(ErrorCode.LOGIN2222_FAILED)
-//        );
-//    }
 }
