0c460dbe3f11c6b6ed29b774167d633c8dd4492e
heehyun0221 <psr4580@gmail.com>
2025-02-12 16:07:04 +0900

    🚑 fix : ValidationErrorMessage 메시지 수정

--- src/main/java/org/team1/keyduck/common/util/ValidationErrorMessage.java
+++ src/main/java/org/team1/keyduck/common/util/ValidationErrorMessage.java
@@ -6,10 +6,10 @@ import lombok.Getter;
 public class ValidationErrorMessage {
 
     public static final String NAME_IS_NOT_NULL = "이름은 필수 입력 값입니다.";
-    public static final String EMAIL_IS_NOT_NULL = "이메일은 비울 수 없습니다.";
+    public static final String EMAIL_IS_NOT_NULL = "이메일은 필수 입력 값입니다.";
     public static final String EMAIL_IS_NOT_VALID = "이메일 형식이 맞지 않습니다.";
     public static final String PASSWORD_IS_NOT_AVAILABLE = "비밀번호 형식이 맞지 않습니다.";
-    public static final String PASSWORD_IS_NOT_NULL = "비밀번호는 비울 수 없습니다.";
-    public static final String MEMBER_ROLE_IS_NOT_NULL = "역할은 필수로 지정해야합니다.";
+    public static final String PASSWORD_IS_NOT_NULL = "비밀번호는 필수 입력 값입니다.";
+    public static final String MEMBER_ROLE_IS_NOT_NULL = "역할은 필수 입력 값입니다.";
 
 }
