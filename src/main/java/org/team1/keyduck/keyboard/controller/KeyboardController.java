fd75114e9e62b5c336db83dd757b3669edc7e699
JinsolSong <jeanies@naver.com>
2025-02-13 14:27:07 +0900

    🚑 fix : url 변경, sellerId 받아오는 코드 변경

--- src/main/java/org/team1/keyduck/keyboard/controller/KeyboardController.java
+++ src/main/java/org/team1/keyduck/keyboard/controller/KeyboardController.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.team1.keyduck.auth.entity.AuthMember;
 import org.team1.keyduck.common.dto.ApiResponse;
 import org.team1.keyduck.common.exception.SuccessCode;
 import org.team1.keyduck.keyboard.dto.response.KeyboardReadResponseDto;
@@ -14,7 +15,7 @@ import org.team1.keyduck.keyboard.service.KeyboardService;
 import java.util.List;
 
 @RestController
-@RequestMapping("keyboards")
+@RequestMapping("/api/keyboards")
 @RequiredArgsConstructor
 public class KeyboardController {
 
@@ -24,7 +25,9 @@ public class KeyboardController {
     @GetMapping("/mine")
     public ResponseEntity<ApiResponse<List<KeyboardReadResponseDto>>> findKeyboardBySellerId(Authentication authentication) {
 
-        Long sellerId = (Long) authentication.getDetails();
+        AuthMember authMember = (AuthMember) authentication.getPrincipal();
+
+        Long sellerId = authMember.getId();
 
         List<KeyboardReadResponseDto> keyboardReadResponseDto = keyboardService.findKeyboardBySellerIdService(sellerId);
 
