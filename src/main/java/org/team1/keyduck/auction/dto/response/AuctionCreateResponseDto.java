bcc44602958f3c100a3d40d12cd70719af370ffa
JinsolSong <jeanies@naver.com>
2025-02-13 22:46:13 +0900

    ✨ feat : 경매 생성 기능 //todo참고 / ErrorCode 추후 변경 예정

--- src/main/java/org/team1/keyduck/auction/dto/response/AuctionCreateResponseDto.java
+++ src/main/java/org/team1/keyduck/auction/dto/response/AuctionCreateResponseDto.java
@@ -1,8 +1,45 @@
 package org.team1.keyduck.auction.dto.response;
 
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.team1.keyduck.auction.entity.Auction;
+import org.team1.keyduck.auction.entity.AuctionStatus;
 
 @Getter
+@AllArgsConstructor
 public class AuctionCreateResponseDto {
+
+    private Long keyboardId;
+
+    private String title;
+
+    private Long startPrice;
+
+    private Long immediatePurchasePrice;
+
+    private int biddingUnit;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime biddingStartDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime biddingEndDate;
+
+    private AuctionStatus auctionStatus;
+
+    public static AuctionCreateResponseDto of(Auction auction) {
+        return new AuctionCreateResponseDto(
+                auction.getKeyboard().getId(),
+                auction.getTitle(),
+                auction.getStartPrice(),
+                auction.getImmediatePurchasePrice(),
+                auction.getBiddingUnit(),
+                auction.getBiddingStartDate(),
+                auction.getBiddingEndDate(),
+                auction.getAuctionStatus()
+        );
+    }
 
 }
