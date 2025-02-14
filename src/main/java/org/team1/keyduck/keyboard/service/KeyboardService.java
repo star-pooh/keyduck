adb34f511051bc3163df6acb56c6b42f3966c38c
JinsolSong <jeanies@naver.com>
2025-02-14 12:39:36 +0900

    🚑 fix : PR 피드백 내용 수정 1. @AuthenticationPrincipal 적용 2. @Transactional(readOnly = true) 적용

--- src/main/java/org/team1/keyduck/keyboard/service/KeyboardService.java
+++ src/main/java/org/team1/keyduck/keyboard/service/KeyboardService.java
@@ -1,12 +1,12 @@
 package org.team1.keyduck.keyboard.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.team1.keyduck.keyboard.dto.response.KeyboardReadResponseDto;
 import org.team1.keyduck.keyboard.entity.Keyboard;
 import org.team1.keyduck.keyboard.repository.KeyboardRepository;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +14,7 @@ public class KeyboardService {
 
     private final KeyboardRepository keyboardRepository;
 
+    @Transactional(readOnly = true)
     public List<KeyboardReadResponseDto> findKeyboardBySellerIdService(Long sellerId) {
 
         List<Keyboard> keyboards = keyboardRepository.findAllByMemberId(sellerId);
