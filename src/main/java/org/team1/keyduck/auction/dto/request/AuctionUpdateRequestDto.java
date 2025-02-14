2f20d539db7c26bf83bb932d84ddd41dcc985caa
JinsolSong <jeanies@naver.com>
2025-02-14 18:05:05 +0900

    🚑 fix : 오류 수정 LocalDateTime 포스트맨에 입력시 ms단위 입력을 하지 않기위해 사용한 어노테이션 오류 수정

--- src/main/java/org/team1/keyduck/auction/dto/request/AuctionUpdateRequestDto.java
+++ src/main/java/org/team1/keyduck/auction/dto/request/AuctionUpdateRequestDto.java
@@ -1,12 +1,12 @@
 package org.team1.keyduck.auction.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import lombok.Getter;
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 public class AuctionUpdateRequestDto {
@@ -24,11 +24,11 @@ public class AuctionUpdateRequestDto {
     private int biddingUnit;
 
     @NotNull(message = "경매 시작일은 필수 입력 값입니다.")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime auctionStartDate;
 
     @Future
     @NotNull(message = "경매 종료일은 필수 입력 값입니다.")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime auctionEndDate;
 }
