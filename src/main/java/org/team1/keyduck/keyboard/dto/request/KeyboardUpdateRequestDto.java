ea56ee436f8e3f33d74c70e6d0b25d4c781be57b
JinsolSong <jeanies@naver.com>
2025-02-15 22:34:19 +0900

    🚑 fix : @Vaild 어노테이션을 위한 RequestDto 수정

--- src/main/java/org/team1/keyduck/keyboard/dto/request/KeyboardUpdateRequestDto.java
+++ src/main/java/org/team1/keyduck/keyboard/dto/request/KeyboardUpdateRequestDto.java
@@ -1,5 +1,6 @@
 package org.team1.keyduck.keyboard.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,8 +8,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public class KeyboardUpdateRequestDto {
 
+    @NotBlank(message = "상품 이름을 입력해주세요.")
     private String name;
 
+    @NotBlank(message = "상품 설명을 입력해주세요.")
     private String description;
 
 }
