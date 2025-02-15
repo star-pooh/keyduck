d140b93fe22a3a7fb36dcc67a470e09a99ce06e2
Jiwon <may0399@naver.com>
2025-02-15 18:49:03 +0900

    Fix : 생성자 private으로 변경

--- src/main/java/org/team1/keyduck/auction/dto/response/AuctionReadResponseDto.java
+++ src/main/java/org/team1/keyduck/auction/dto/response/AuctionReadResponseDto.java
@@ -1,13 +1,11 @@
 package org.team1.keyduck.auction.dto.response;
 
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.team1.keyduck.auction.entity.Auction;
 import org.team1.keyduck.auction.entity.AuctionStatus;
 
 @Getter
-@AllArgsConstructor
 public class AuctionReadResponseDto {
     private String title;
     private Long startPrice;
@@ -17,6 +15,20 @@ public class AuctionReadResponseDto {
     private LocalDateTime biddingStartDate;
     private LocalDateTime biddingEndDate;
     private AuctionStatus auctionStatus;
+
+    private AuctionReadResponseDto(String title, Long startPrice, Long currentPrice,
+            Long immediatePurchasePrice, int biddingUnit, LocalDateTime biddingStartDate,
+            LocalDateTime biddingEndDate, AuctionStatus auctionStatus
+    ) {
+        this.title = title;
+        this.startPrice = startPrice;
+        this.currentPrice = currentPrice;
+        this.immediatePurchasePrice = immediatePurchasePrice;
+        this.biddingUnit = biddingUnit;
+        this.biddingStartDate = biddingStartDate;
+        this.biddingEndDate = biddingEndDate;
+        this.auctionStatus = auctionStatus;
+    }
 
     public static AuctionReadResponseDto of(Auction auction) {
         return new AuctionReadResponseDto(
