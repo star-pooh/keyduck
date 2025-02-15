949ab2623a55f029f61cae54612ffe4e66d667a9
t2023-m0014 <qa9377@gmail.com>
2025-02-16 00:01:05 +0900

    fix: 에러코드 충돌 해결

--- src/main/java/org/team1/keyduck/common/exception/GlobalExceptionHandler.java
+++ src/main/java/org/team1/keyduck/common/exception/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.team1.keyduck.common.dto.ApiResponse;
@@ -53,16 +52,6 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(apiResponse, apiResponse.getStatus());
     }
 
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResponseEntity<ApiResponse> handleMissingParamException(
-            MissingServletRequestParameterException ex) {
-        if ("price".equals(ex.getParameterName())) {
-            ApiResponse apiResponse = ApiResponse.error(ErrorCode.BIDDING_PRICE_IS_NULL);
-            return new ResponseEntity<>(apiResponse, apiResponse.getStatus());
-        }
-        ApiResponse apiResponse = ApiResponse.error(ErrorCode.INVALID_INPUT_VALUE);
-        return new ResponseEntity<>(apiResponse, apiResponse.getStatus());
-    }
 
     @ExceptionHandler(InvalidBiddingPriceException.class)
     public ResponseEntity<ApiResponse> handleInvalidBiddingPriceException(
@@ -82,7 +71,8 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(DataMismatchException.class)
-    public ResponseEntity<ApiResponse> handleDataMismatchException(DataMismatchException exception) {
+    public ResponseEntity<ApiResponse> handleDataMismatchException(
+            DataMismatchException exception) {
         ApiResponse apiResponse = ApiResponse.error(ErrorCode.FORBIDDEN_ACCESS);
         log.info("{}, {}, {}", apiResponse.getCode(), exception.getStackTrace(),
                 apiResponse.getMessage());
