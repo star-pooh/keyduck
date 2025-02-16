b21debdc76c2e4d200b6a7e54eb075a33b720de0
Jiwon <may0399@naver.com>
2025-02-16 16:54:50 +0900

    Fix : 충돌해결

--- src/main/java/org/team1/keyduck/auction/dto/response/AuctionReadResponseDto.java
+++ src/main/java/org/team1/keyduck/auction/dto/response/AuctionReadResponseDto.java
@@ -7,16 +7,28 @@ import org.team1.keyduck.auction.entity.Auction;
 import org.team1.keyduck.auction.entity.AuctionStatus;
 
 @Getter
-@AllArgsConstructor
 public class AuctionReadResponseDto {
     private String title;
     private Long startPrice;
     private Long currentPrice;
     private Long immediatePurchasePrice;
     private int biddingUnit;
-    private LocalDateTime biddingStartDate;
-    private LocalDateTime biddingEndDate;
+    private LocalDateTime auctionStartDate;
+    private LocalDateTime auctionEndDate;
     private AuctionStatus auctionStatus;
+
+    private AuctionReadResponseDto(String title, Long startPrice, Long currentPrice,
+            Long immediatePurchasePrice, int biddingUnit, LocalDateTime auctionStartDate,
+            LocalDateTime auctionEndDate, AuctionStatus auctionStatus) {
+        this.title = title;
+        this.startPrice = startPrice;
+        this.currentPrice = currentPrice;
+        this.immediatePurchasePrice = immediatePurchasePrice;
+        this.biddingUnit = biddingUnit;
+        this.auctionStartDate = auctionStartDate;
+        this.auctionEndDate = auctionEndDate;
+        this.auctionStatus = auctionStatus;
+    }
 
     public static AuctionReadResponseDto of(Auction auction) {
         return new AuctionReadResponseDto(
@@ -25,8 +37,8 @@ public class AuctionReadResponseDto {
                 auction.getCurrentPrice(),
                 auction.getImmediatePurchasePrice(),
                 auction.getBiddingUnit(),
-                auction.getBiddingStartDate(),
-                auction.getBiddingEndDate(),
+                auction.getAuctionStartDate(),
+                auction.getAuctionEndDate(),
                 auction.getAuctionStatus()
         );
     }
--- src/main/java/org/team1/keyduck/auction/service/AuctionService.java
+++ src/main/java/org/team1/keyduck/auction/service/AuctionService.java
@@ -4,15 +4,71 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.team1.keyduck.auction.dto.request.AuctionCreateRequestDto;
+import org.team1.keyduck.auction.dto.request.AuctionUpdateRequestDto;
+import org.team1.keyduck.auction.dto.response.AuctionCreateResponseDto;
 import org.team1.keyduck.auction.dto.response.AuctionReadAllResponseDto;
+import org.team1.keyduck.auction.dto.response.AuctionUpdateResponseDto;
 import org.team1.keyduck.auction.entity.Auction;
+import org.team1.keyduck.auction.entity.AuctionStatus;
 import org.team1.keyduck.auction.repository.AuctionRepository;
+import org.team1.keyduck.common.exception.DataNotFoundException;
+import org.team1.keyduck.common.exception.DataNotMatchException;
+import org.team1.keyduck.common.exception.ErrorCode;
+import org.team1.keyduck.keyboard.entity.Keyboard;
+import org.team1.keyduck.keyboard.repository.KeyboardRepository;
 
 @Service
 @RequiredArgsConstructor
 public class AuctionService {
 
     private final AuctionRepository auctionRepository;
+    private final KeyboardRepository keyboardRepository;
+
+    public AuctionCreateResponseDto createAuctionService(Long sellerId,
+            AuctionCreateRequestDto requestDto) {
+        //todo ErrorCode 추후에 키보드를 찾을 수 없다는 내용의 에러 코드 추가 후 익셉션 에러코드 변경이 필요함.
+        Keyboard findKeyboard = keyboardRepository.findById(requestDto.getKeyboardId())
+                .orElseThrow(() -> new DataNotFoundException(ErrorCode.RESOURCE_NOT_FOUND));
+
+        if (!findKeyboard.getMember().getId().equals(sellerId)) {
+            throw new DataNotMatchException(ErrorCode.FORBIDDEN_ACCESS);
+        }
+
+        Auction auction = Auction.builder()
+                .keyboard(findKeyboard)
+                .member(findKeyboard.getMember())
+                .title(requestDto.getTitle())
+                .startPrice(requestDto.getStartPrice())
+                .immediatePurchasePrice(requestDto.getImmediatePurchasePrice())
+                .currentPrice(requestDto.getStartPrice())
+                .biddingUnit(requestDto.getBiddingUnit())
+                .auctionStartDate(requestDto.getAuctionStartDate())
+                .auctionEndDate(requestDto.getAuctionEndDate())
+                .auctionStatus(AuctionStatus.NOT_STARTED)
+                .build();
+
+        Auction saveAuction = auctionRepository.save(auction);
+
+        return AuctionCreateResponseDto.of(saveAuction);
+
+    }
+
+    @Transactional
+    public AuctionUpdateResponseDto auctionModification(Long sellerId, Long auctionId,
+            AuctionUpdateRequestDto requestDto) {
+
+        //todo 추후에 경매정보를 찾을 수 없다는 내용의 에러 코드 추가 후 익셉션 에러코드 변경이 필요함.
+        Auction findAuction = auctionRepository.findById(auctionId)
+                .orElseThrow(() -> new DataNotFoundException(ErrorCode.RESOURCE_NOT_FOUND));
+
+        if (!findAuction.getMember().getId().equals(sellerId)) {
+            throw new DataNotMatchException(ErrorCode.FORBIDDEN_ACCESS);
+        }
+        findAuction.updateAuction(requestDto);
+
+        return AuctionUpdateResponseDto.of(findAuction);
+    }
 
     // 경매 다건 조회
     @Transactional(readOnly = true)
