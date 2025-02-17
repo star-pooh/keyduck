ec0a10d73bfcfc8fa8141b15f75482553d2fc9c9
JinsolSong <jeanies@naver.com>
2025-02-17 10:07:16 +0900

    🚑 fix : 키보드 조회기능 수정 삭제된 키보드가 조회되지 않도록 코드 수정

--- src/main/java/org/team1/keyduck/keyboard/repository/KeyboardRepository.java
+++ src/main/java/org/team1/keyduck/keyboard/repository/KeyboardRepository.java
@@ -1,11 +1,10 @@
 package org.team1.keyduck.keyboard.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.team1.keyduck.keyboard.entity.Keyboard;
 
-import java.util.List;
-
 public interface KeyboardRepository extends JpaRepository<Keyboard, Long> {
 
-    List<Keyboard> findAllByMemberId(Long sellerId);
+    List<Keyboard> findAllByMemberIdAndIsDeletedFalse(Long sellerId);
 }
