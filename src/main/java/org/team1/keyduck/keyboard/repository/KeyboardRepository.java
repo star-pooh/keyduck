f0c6b194d2a6cb1286a7c6913c187165838f7862
JinsolSong <jeanies@naver.com>
2025-02-17 16:44:11 +0900

    🚑 fix : 키보드 찾는 코드 수정 키보드를 찾아서 if문을 통해 isDeleted상태를 검증하는 로직을 쿼리메소드로 변경

--- src/main/java/org/team1/keyduck/keyboard/repository/KeyboardRepository.java
+++ src/main/java/org/team1/keyduck/keyboard/repository/KeyboardRepository.java
@@ -1,11 +1,15 @@
 package org.team1.keyduck.keyboard.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.team1.keyduck.keyboard.entity.Keyboard;
-
 import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.team1.keyduck.keyboard.entity.Keyboard;
 
 public interface KeyboardRepository extends JpaRepository<Keyboard, Long> {
 
     List<Keyboard> findAllByMemberId(Long sellerId);
+
+    @Query("SELECT k FROM Keyboard k WHERE k.id = :keyboardId AND k.isDeleted = false")
+    Optional<Keyboard> findByIdAndIsDeletedFalse(Long keyboardId);
 }
