c59130ad24fd1bf9e1e09f45e0c6333d12c09c53
Jiwon <may0399@naver.com>
2025-02-18 16:06:40 +0900

    Refactor : 키보드 DTO 에러 코드 및 메시지 리팩토링

--- src/main/java/org/team1/keyduck/common/util/ValidationErrorMessage.java
+++ src/main/java/org/team1/keyduck/common/util/ValidationErrorMessage.java
@@ -12,6 +12,7 @@ public class ValidationErrorMessage {
     public static final String PASSWORD_IS_NOT_NULL = "비밀번호은(는) 필수 입력 값입니다.";
     public static final String MEMBER_ROLE_IS_NOT_NULL = "역할은(는) 필수 입력 값입니다.";
     public static final String ADDRESS_IS_NOT_NULL = "주소은(는) 필수 입력 값입니다.";
+    public static final String DESCRIPTION_IS_NOT_NULL = "내용은(는) 필수 입력 값입니다.";
     
 
 }
--- src/main/java/org/team1/keyduck/keyboard/dto/request/KeyboardUpdateRequestDto.java
+++ src/main/java/org/team1/keyduck/keyboard/dto/request/KeyboardUpdateRequestDto.java
@@ -3,15 +3,16 @@ package org.team1.keyduck.keyboard.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.team1.keyduck.common.util.ValidationErrorMessage;
 
 @Getter
 @AllArgsConstructor
 public class KeyboardUpdateRequestDto {
 
-    @NotBlank(message = "상품 이름을 입력해주세요.")
+    @NotBlank(message = ValidationErrorMessage.NAME_IS_NOT_NULL)
     private String name;
 
-    @NotBlank(message = "상품 설명을 입력해주세요.")
+    @NotBlank(message = ValidationErrorMessage.DESCRIPTION_IS_NOT_NULL)
     private String description;
 
 }
