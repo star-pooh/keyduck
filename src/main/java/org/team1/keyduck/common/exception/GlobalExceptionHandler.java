b59764eab0bdd83351095696ae41ba3999197eb9
t2023-m0014 <qa9377@gmail.com>
2025-02-18 18:08:00 +0900

    :rewind: rewind GlobalExceptionHandler

--- src/main/java/org/team1/keyduck/common/exception/GlobalExceptionHandler.java
+++ src/main/java/org/team1/keyduck/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.team1.keyduck.common.exception;
 
+import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.team1.keyduck.common.dto.ApiResponse;
-import org.team1.keyduck.common.util.ErrorMessage;
 
 @Slf4j
 @RestControllerAdvice
@@ -87,16 +87,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(HandlerMethodValidationException.class)
     public ResponseEntity<ApiResponse> handleHandlerMethodValidationExceptionException(
             HandlerMethodValidationException exception) {
-        String fieldName = "";
-        Object[] arguments = exception.getDetailMessageArguments();
-        if (arguments != null && arguments.length > 0
-                && arguments[0] instanceof String extractedField) {
-            if ("price".equals(extractedField)) {
-                fieldName = "입찰가격";
-            }
-        }
-        String errorMessage = String.format(ErrorMessage.REQUIRED_VALUE, fieldName);
-        ApiResponse apiResponse = ApiResponse.error(ErrorCode.REQUIRED_VALUE, errorMessage);
+        ApiResponse apiResponse = ApiResponse.error(ErrorCode.INVALID_DATA_VALUE,
+                Arrays.toString(exception.getDetailMessageArguments()));
         log.info("{}, {}, {}", apiResponse.getCode(), exception.getStackTrace(),
                 apiResponse.getMessage());
         return new ResponseEntity<>(apiResponse, apiResponse.getStatus());
