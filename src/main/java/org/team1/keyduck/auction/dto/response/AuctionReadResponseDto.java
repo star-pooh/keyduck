599428b5cca577ad427d625d58df3712e5b8d5ca
JinsolSong <jeanies@naver.com>
2025-02-18 09:39:47 +0900

    🚑 fix : 응답, 반환값 통일을 위한DTO수정 각 도메인별 응답, 반환값 통일은 위한 DTO 수정

--- src/main/java/org/team1/keyduck/auction/dto/response/AuctionReadResponseDto.java
+++ src/main/java/org/team1/keyduck/auction/dto/response/AuctionReadResponseDto.java
@@ -1,5 +1,6 @@
 package org.team1.keyduck.auction.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Getter;
@@ -9,12 +10,15 @@ import org.team1.keyduck.bidding.dto.response.BiddingResponseDto;
 
 @Getter
 public class AuctionReadResponseDto {
+
     private String title;
     private Long startPrice;
     private Long currentPrice;
     private Long immediatePurchasePrice;
     private int biddingUnit;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime auctionStartDate;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime auctionEndDate;
     private AuctionStatus auctionStatus;
     private List<BiddingResponseDto> biddings;
@@ -22,7 +26,8 @@ public class AuctionReadResponseDto {
 
     private AuctionReadResponseDto(String title, Long startPrice, Long currentPrice,
             Long immediatePurchasePrice, int biddingUnit, LocalDateTime auctionStartDate,
-            LocalDateTime auctionEndDate, AuctionStatus auctionStatus, List<BiddingResponseDto> biddings) {
+            LocalDateTime auctionEndDate, AuctionStatus auctionStatus,
+            List<BiddingResponseDto> biddings) {
         this.title = title;
         this.startPrice = startPrice;
         this.currentPrice = currentPrice;
