a0b7acc8dc9cd8dc6e406217022f7d560a53aee7
star-pooh <eunki92@gmail.com>
2025-02-20 12:47:52 +0900

    test : test feature

--- src/main/java/org/team1/keyduck/keyboard/service/KeyboardService.java
+++ src/main/java/org/team1/keyduck/keyboard/service/KeyboardService.java
@@ -29,6 +29,8 @@ public class KeyboardService {
     private final MemberRepository memberRepository;
     private final AuctionRepository auctionRepository;
 
+    private Long testLong;
+
     // 키보드 생성
     @Transactional
     public KeyboardCreateResponseDto createKeyboard(Long memberId,
