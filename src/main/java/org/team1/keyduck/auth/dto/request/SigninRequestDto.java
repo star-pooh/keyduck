cb8e3f22f4f85faf1a8d95af7f5c0129826fb030
heehyun0221 <psr4580@gmail.com>
2025-02-21 20:09:12 +0900

    🚑 fix : 로그인 시 이메일 필드 검증 로직 통일

--- src/main/java/org/team1/keyduck/auth/dto/request/SigninRequestDto.java
+++ src/main/java/org/team1/keyduck/auth/dto/request/SigninRequestDto.java
@@ -1,6 +1,5 @@
 package org.team1.keyduck.auth.dto.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -14,7 +13,7 @@ import org.team1.keyduck.common.util.ValidationErrorMessage;
 public class SigninRequestDto {
 
     @NotBlank(message = ValidationErrorMessage.EMAIL_IS_NOT_NULL)
-    @Email(message = ValidationErrorMessage.EMAIL_IS_NOT_VALID)
+    @Pattern(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$", message = ValidationErrorMessage.EMAIL_IS_NOT_VALID)
     private String email;
 
     @NotBlank(message = ValidationErrorMessage.PASSWORD_IS_NOT_NULL)
