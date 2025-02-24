b12d46a5f5349318d3dfb539b22385c285e97e06
Jiwon <may0399@naver.com>
2025-02-24 16:47:21 +0900

    Fix : 서비스 로직 수정에 따른 테스트코드 수정 및 추가

--- src/test/java/org/team1/keyduck/keyboard/service/KeyboardServiceTest.java
+++ src/test/java/org/team1/keyduck/keyboard/service/KeyboardServiceTest.java
@@ -48,61 +48,61 @@ class KeyboardServiceTest {
     @InjectMocks
     private KeyboardService keyboardService;
 
-//    @Test
-//    @DisplayName("키보드 조회_성공_키보드가 1개일때")
-//    public void findKeyboard_success_one() {
-//        //given : 설정(테스트준비)
-//        Member member = TEST_MEMBER1;
-//        Keyboard keyboard = TEST_KEYBOARD1;
-//
-//        ReflectionTestUtils.setField(member, "id", TEST_ID1);
-//
-//        //when : 수행할 작업(테스트 검증을 위한 준비)
-//        when(keyboardRepository.findAllByMemberId(member.getId())).thenReturn(List.of(keyboard));
-//        //then : 결과검증
-//        List<KeyboardReadResponseDto> result = keyboardService.findKeyboardBySellerId(
-//                member.getId());
-//
-//        assertEquals(1, result.size());
-//        assertEquals(keyboard.getId(), result.get(0).getId());
-//    }
-//
-//    @Test
-//    @DisplayName("키보드 조회_성공_키보드가 여러개일때")
-//    public void findKeyboard_success_many() {
-//
-//        //given : 설정
-//        Member member = TEST_MEMBER1;
-//
-//        List<Keyboard> keyboardList = List.of(TestData.TEST_KEYBOARD1, TEST_KEYBOARD2,
-//                TEST_KEYBOARD3);
-//
-//        //when : 수행할 작업
-//        when(keyboardRepository.findAllByMemberId(member.getId())).thenReturn(keyboardList);
-//
-//        List<KeyboardReadResponseDto> result = keyboardService.findKeyboardBySellerId(
-//                member.getId());
-//
-//        //then : 결과검증
-//        assertEquals(3, result.size());
-//    }
-//
-//    @Test
-//    @DisplayName("키보드 조회_키보드가 없을때")
-//    public void findKeyboard_empty() {
-//
-//        //given : 설정
-//        Member member = TEST_MEMBER1;
-//
-//        //when : 수행할 작업
-//        when(keyboardRepository.findAllByMemberId(member.getId())).thenReturn(
-//                Collections.emptyList());
-//
-//        //then : 결과검증
-//        List<KeyboardReadResponseDto> result = keyboardService.findKeyboardBySellerId(TEST_ID1);
-//
-//        assertTrue(result.isEmpty());
-//    }
+    @Test
+    @DisplayName("키보드 조회_성공_키보드가 1개일때")
+    public void findKeyboard_success_one() {
+        //given : 설정(테스트준비)
+        Member member = TEST_MEMBER1;
+        Keyboard keyboard = TEST_KEYBOARD1;
+
+        ReflectionTestUtils.setField(member, "id", TEST_ID1);
+
+        //when : 수행할 작업(테스트 검증을 위한 준비)
+        when(keyboardRepository.findAllByMemberId(member.getId())).thenReturn(List.of(keyboard));
+        //then : 결과검증
+        List<KeyboardReadResponseDto> result = keyboardService.findKeyboardBySellerId(
+                member.getId());
+
+        assertEquals(1, result.size());
+        assertEquals(keyboard.getId(), result.get(0).getId());
+    }
+
+    @Test
+    @DisplayName("키보드 조회_성공_키보드가 여러개일때")
+    public void findKeyboard_success_many() {
+
+        //given : 설정
+        Member member = TEST_MEMBER1;
+
+        List<Keyboard> keyboardList = List.of(TestData.TEST_KEYBOARD1, TEST_KEYBOARD2,
+                TEST_KEYBOARD3);
+
+        //when : 수행할 작업
+        when(keyboardRepository.findAllByMemberId(member.getId())).thenReturn(keyboardList);
+
+        List<KeyboardReadResponseDto> result = keyboardService.findKeyboardBySellerId(
+                member.getId());
+
+        //then : 결과검증
+        assertEquals(3, result.size());
+    }
+
+    @Test
+    @DisplayName("키보드 조회_키보드가 없을때")
+    public void findKeyboard_empty() {
+
+        //given : 설정
+        Member member = TEST_MEMBER1;
+
+        //when : 수행할 작업
+        when(keyboardRepository.findAllByMemberId(member.getId())).thenReturn(
+                Collections.emptyList());
+
+        //then : 결과검증
+        List<KeyboardReadResponseDto> result = keyboardService.findKeyboardBySellerId(TEST_ID1);
+
+        assertTrue(result.isEmpty());
+    }
 
     // 가짜 객체
     @Mock
