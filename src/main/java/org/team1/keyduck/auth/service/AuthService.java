5c63826e7b50160e1a16d0b38818c3ce5a3ead05
heehyun0221 <psr4580@gmail.com>
2025-02-26 23:45:55 +0900

    🚑 fix : 파라미터 수정

--- src/main/java/org/team1/keyduck/auth/service/AuthService.java
+++ src/main/java/org/team1/keyduck/auth/service/AuthService.java
@@ -34,7 +34,8 @@ public class AuthService {
 
     public SigninResponseDto login(SigninRequestDto signinRequest) {
         Member member = memberRepository.findByEmail(signinRequest.getEmail())
-                .orElseThrow(() -> new DataNotFoundException(ErrorCode.INVALID_DATA_VALUE, "이메일"));
+                .orElseThrow(() -> new DataNotFoundException(ErrorCode.INVALID_DATA_VALUE,
+                        ErrorMessageParameter.EMAIL));
 
         if (member.isDeleted()) {
             throw new DataInvalidException(ErrorCode.DUPLICATE_DELETED,
