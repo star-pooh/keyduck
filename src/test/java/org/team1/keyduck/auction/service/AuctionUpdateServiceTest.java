3f36e38c87c1534fd3b68249800e073f6070b6cc
JinsolSong <jeanies@naver.com>
2025-03-04 12:05:34 +0900

    ✅ test : PR코멘트 내용 반영하여 수정

--- src/test/java/org/team1/keyduck/auction/service/AuctionUpdateServiceTest.java
+++ src/test/java/org/team1/keyduck/auction/service/AuctionUpdateServiceTest.java
@@ -1,11 +1,12 @@
 package org.team1.keyduck.auction.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,7 +16,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.team1.keyduck.auction.dto.request.AuctionUpdateRequestDto;
-import org.team1.keyduck.auction.dto.response.AuctionUpdateResponseDto;
 import org.team1.keyduck.auction.entity.Auction;
 import org.team1.keyduck.auction.entity.AuctionStatus;
 import org.team1.keyduck.auction.repository.AuctionRepository;
@@ -37,72 +37,90 @@ class AuctionUpdateServiceTest {
 
     @Test
     @DisplayName("경매 수정 _성공")
-    public void auction_update_success() {
+    public void auctionUpdateSuccess() {
 
         Member member = mock(Member.class);
-        Auction auction = mock(Auction.class);
         Keyboard keyboard = mock(Keyboard.class);
+
+        Auction auction = Auction.builder()
+                .keyboard(keyboard)
+                .member(null)
+                .title(TestData.TEST_AUCTION_TITLE)
+                .startPrice(TestData.TEST_AUCTION_START_PRICE1)
+                .immediatePurchasePrice(TestData.TEST_AUCTION_IMMEDIATE_PURCHASE_PRICE1)
+                .currentPrice(TestData.TEST_AUCTION_START_PRICE1)
+                .biddingUnit(TestData.TEST_AUCTION_BIDDING_UNIT1)
+                .auctionStartDate(LocalDateTime.now().plusDays(1))
+                .auctionEndDate(LocalDateTime.now().plusDays(3))
+                .auctionStatus(AuctionStatus.NOT_STARTED)
+                .build();
 
         AuctionUpdateRequestDto requestDto = new AuctionUpdateRequestDto();
 
-        ReflectionTestUtils.setField(requestDto, "title", TestData.TEST_AUCTION_TITLE2);
+        ReflectionTestUtils.setField(requestDto, "title", "제목 변경");
 
         when(auctionRepository.findById(auction.getId())).thenReturn(Optional.of(auction));
-        when(auction.getKeyboard()).thenReturn(keyboard);
-        when(keyboard.getMember()).thenReturn(member);
+        when(auction.getKeyboard().getMember()).thenReturn(member);
         when(auction.getKeyboard().getMember().getId()).thenReturn(1L);
-        when(auction.getAuctionStatus()).thenReturn(AuctionStatus.NOT_STARTED);
 
-        AuctionUpdateResponseDto responseDto = auctionService.auctionModification(
-                member.getId(), auction.getId(), requestDto);
+        auctionService.auctionModification(member.getId(), auction.getId(), requestDto);
 
-        assertNotNull(requestDto);
-        assertEquals(requestDto.getTitle(), TestData.TEST_AUCTION_TITLE2);
+        assertEquals(auction.getTitle(), "제목 변경");
     }
 
     @Test
     @DisplayName("경매 수정_실패_경매_없음")
-    public void auction_update_fail_not_found_auction() {
+    public void auctionUpdateFailNotFoundAuction() {
 
-        Auction auction = mock(Auction.class);
-        Member member = mock(Member.class);
-        AuctionUpdateRequestDto requestDto = new AuctionUpdateRequestDto();
+        Long auctionId = 1L;
+        Long sellerId = 1L;
 
-        when(auctionRepository.findById(auction.getId())).thenReturn(Optional.empty());
+        AuctionUpdateRequestDto requestDto = mock(AuctionUpdateRequestDto.class);
+
+        ReflectionTestUtils.setField(requestDto, "title", "제목 변경");
+
+        when(auctionRepository.findById(any(Long.class))).thenReturn(Optional.empty());
 
         DataNotFoundException e = assertThrows(DataNotFoundException.class,
                 () -> auctionService.auctionModification(
-                        member.getId(), auction.getId(), requestDto));
+                        sellerId, auctionId, requestDto));
 
         assertEquals("해당 경매을(를) 찾을 수 없습니다.", e.getMessage());
     }
 
     @Test
     @DisplayName("경매 수정_실패_경매_진행중")
-    public void auction_update_fail_auction_in_progress() {
+    public void auctionUpdateFailAuctionInProgress() {
 
         Auction auction = mock(Auction.class);
-        Member member = mock(Member.class);
-        AuctionUpdateRequestDto requestDto = new AuctionUpdateRequestDto();
 
-        when(auctionRepository.findById(auction.getId())).thenReturn(Optional.of(auction));
+        Long sellerId = 1L;
+
+        AuctionUpdateRequestDto requestDto = mock(AuctionUpdateRequestDto.class);
+
+        ReflectionTestUtils.setField(requestDto, "title", "제목 변경");
+        ReflectionTestUtils.setField(auction, "id", 1L);
+
+        when(auctionRepository.findById(any(Long.class))).thenReturn(Optional.of(auction));
         when(auction.getAuctionStatus()).thenReturn(AuctionStatus.IN_PROGRESS);
 
         DataInvalidException e = assertThrows(DataInvalidException.class,
                 () -> auctionService.auctionModification(
-                        member.getId(), auction.getId(), requestDto));
+                        sellerId, auction.getId(), requestDto));
 
         assertEquals("유효하지 않은 경매 상태 입니다.", e.getMessage());
     }
 
     @Test
     @DisplayName("경매 수정_실패_경매를 등록한 셀러가 아님")
-    public void auction_update_fail_Unauthorized() {
+    public void auctionUpdateFailUnauthorized() {
 
         Auction auction = mock(Auction.class);
         Member member = mock(Member.class);
         Keyboard keyboard = mock(Keyboard.class);
-        AuctionUpdateRequestDto requestDto = new AuctionUpdateRequestDto();
+        AuctionUpdateRequestDto requestDto = mock(AuctionUpdateRequestDto.class);
+
+        ReflectionTestUtils.setField(auction, "id", 1L);
 
         when(auctionRepository.findById(auction.getId())).thenReturn(Optional.of(auction));
         when(auction.getAuctionStatus()).thenReturn(AuctionStatus.NOT_STARTED);
@@ -118,6 +136,4 @@ class AuctionUpdateServiceTest {
 
         assertEquals("접근 권한이 없습니다.", e.getMessage());
     }
-
-
 }
